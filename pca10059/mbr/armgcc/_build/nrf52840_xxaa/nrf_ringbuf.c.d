83c4989a4087de283bac3c62ee36ede548a0d5d1
TNTrol <sem.go@bk.ru>
2021-10-31 21:18:40 +0300

    create new project/ move example/ delete unneeded fules

---
+++ pca10059/mbr/armgcc/_build/nrf52840_xxaa/nrf_ringbuf.c.d
@@ -0,0 +1,116 @@
+_build/nrf52840_xxaa/nrf_ringbuf.c.o: \
+ ../../../../ESTC-NSDK/components/libraries/ringbuf/nrf_ringbuf.c \
+ ../../../../ESTC-NSDK/components/libraries/ringbuf/nrf_ringbuf.h \
+ /usr/lib/gcc/arm-none-eabi/11.2.0/include/stdint.h \
+ /usr/arm-none-eabi/include/stdint.h \
+ /usr/arm-none-eabi/include/machine/_default_types.h \
+ /usr/arm-none-eabi/include/sys/features.h \
+ /usr/arm-none-eabi/include/_newlib_version.h \
+ /usr/arm-none-eabi/include/sys/_intsup.h \
+ /usr/arm-none-eabi/include/sys/_stdint.h \
+ ../../../../ESTC-NSDK/components/libraries/atomic/nrf_atomic.h \
+ ../../../../ESTC-NSDK/components/libraries/util/sdk_common.h \
+ /usr/lib/gcc/arm-none-eabi/11.2.0/include/stdbool.h \
+ /usr/arm-none-eabi/include/string.h /usr/arm-none-eabi/include/_ansi.h \
+ /usr/arm-none-eabi/include/newlib.h \
+ /usr/arm-none-eabi/include/sys/config.h \
+ /usr/arm-none-eabi/include/machine/ieeefp.h \
+ /usr/arm-none-eabi/include/sys/reent.h \
+ /usr/arm-none-eabi/include/_ansi.h \
+ /usr/lib/gcc/arm-none-eabi/11.2.0/include/stddef.h \
+ /usr/arm-none-eabi/include/sys/_types.h \
+ /usr/arm-none-eabi/include/machine/_types.h \
+ /usr/arm-none-eabi/include/sys/lock.h \
+ /usr/arm-none-eabi/include/sys/cdefs.h \
+ /usr/arm-none-eabi/include/sys/string.h ../config/sdk_config.h \
+ ../../../../ESTC-NSDK/components/libraries/util/nordic_common.h \
+ ../../../../ESTC-NSDK/modules/nrfx/mdk/compiler_abstraction.h \
+ ../../../../ESTC-NSDK/components/libraries/util/sdk_os.h \
+ ../../../../ESTC-NSDK/components/libraries/util/sdk_errors.h \
+ ../../../../ESTC-NSDK/components/drivers_nrf/nrf_soc_nosd/nrf_error.h \
+ ../../../../ESTC-NSDK/components/libraries/util/app_util.h \
+ ../../../../ESTC-NSDK/modules/nrfx/mdk/nrf.h \
+ ../../../../ESTC-NSDK/modules/nrfx/mdk/nrf52840.h \
+ ../../../../ESTC-NSDK/components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../ESTC-NSDK/components/toolchain/cmsis/include/cmsis_version.h \
+ ../../../../ESTC-NSDK/components/toolchain/cmsis/include/cmsis_compiler.h \
+ ../../../../ESTC-NSDK/components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../ESTC-NSDK/components/toolchain/cmsis/include/mpu_armv7.h \
+ ../../../../ESTC-NSDK/modules/nrfx/mdk/system_nrf52840.h \
+ ../../../../ESTC-NSDK/modules/nrfx/mdk/nrf52840_bitfields.h \
+ ../../../../ESTC-NSDK/modules/nrfx/mdk/nrf51_to_nrf52840.h \
+ ../../../../ESTC-NSDK/modules/nrfx/mdk/nrf52_to_nrf52840.h \
+ ../../../../ESTC-NSDK/modules/nrfx/mdk/compiler_abstraction.h \
+ ../../../../ESTC-NSDK/components/softdevice/mbr/headers/nrf_mbr.h \
+ ../../../../ESTC-NSDK/components/softdevice/mbr/headers/nrf_svc.h \
+ ../../../../ESTC-NSDK/components/libraries/util/sdk_macros.h \
+ ../../../../ESTC-NSDK/components/libraries/util/nrf_assert.h \
+ ../../../../ESTC-NSDK/components/libraries/util/sdk_errors.h \
+ ../../../../ESTC-NSDK/components/libraries/util/app_util_platform.h \
+ ../../../../ESTC-NSDK/components/libraries/util/app_error.h \
+ /usr/arm-none-eabi/include/stdio.h \
+ /usr/lib/gcc/arm-none-eabi/11.2.0/include/stdarg.h \
+ /usr/arm-none-eabi/include/sys/types.h \
+ /usr/arm-none-eabi/include/sys/_pthreadtypes.h \
+ /usr/arm-none-eabi/include/machine/types.h \
+ /usr/arm-none-eabi/include/sys/stdio.h \
+ ../../../../ESTC-NSDK/components/libraries/util/app_error_weak.h \
+ ../../../../ESTC-NSDK/components/libraries/util/nrf_assert.h
+../../../../ESTC-NSDK/components/libraries/ringbuf/nrf_ringbuf.h:
+/usr/lib/gcc/arm-none-eabi/11.2.0/include/stdint.h:
+/usr/arm-none-eabi/include/stdint.h:
+/usr/arm-none-eabi/include/machine/_default_types.h:
+/usr/arm-none-eabi/include/sys/features.h:
+/usr/arm-none-eabi/include/_newlib_version.h:
+/usr/arm-none-eabi/include/sys/_intsup.h:
+/usr/arm-none-eabi/include/sys/_stdint.h:
+../../../../ESTC-NSDK/components/libraries/atomic/nrf_atomic.h:
+../../../../ESTC-NSDK/components/libraries/util/sdk_common.h:
+/usr/lib/gcc/arm-none-eabi/11.2.0/include/stdbool.h:
+/usr/arm-none-eabi/include/string.h:
+/usr/arm-none-eabi/include/_ansi.h:
+/usr/arm-none-eabi/include/newlib.h:
+/usr/arm-none-eabi/include/sys/config.h:
+/usr/arm-none-eabi/include/machine/ieeefp.h:
+/usr/arm-none-eabi/include/sys/reent.h:
+/usr/arm-none-eabi/include/_ansi.h:
+/usr/lib/gcc/arm-none-eabi/11.2.0/include/stddef.h:
+/usr/arm-none-eabi/include/sys/_types.h:
+/usr/arm-none-eabi/include/machine/_types.h:
+/usr/arm-none-eabi/include/sys/lock.h:
+/usr/arm-none-eabi/include/sys/cdefs.h:
+/usr/arm-none-eabi/include/sys/string.h:
+../config/sdk_config.h:
+../../../../ESTC-NSDK/components/libraries/util/nordic_common.h:
+../../../../ESTC-NSDK/modules/nrfx/mdk/compiler_abstraction.h:
+../../../../ESTC-NSDK/components/libraries/util/sdk_os.h:
+../../../../ESTC-NSDK/components/libraries/util/sdk_errors.h:
+../../../../ESTC-NSDK/components/drivers_nrf/nrf_soc_nosd/nrf_error.h:
+../../../../ESTC-NSDK/components/libraries/util/app_util.h:
+../../../../ESTC-NSDK/modules/nrfx/mdk/nrf.h:
+../../../../ESTC-NSDK/modules/nrfx/mdk/nrf52840.h:
+../../../../ESTC-NSDK/components/toolchain/cmsis/include/core_cm4.h:
+../../../../ESTC-NSDK/components/toolchain/cmsis/include/cmsis_version.h:
+../../../../ESTC-NSDK/components/toolchain/cmsis/include/cmsis_compiler.h:
+../../../../ESTC-NSDK/components/toolchain/cmsis/include/cmsis_gcc.h:
+../../../../ESTC-NSDK/components/toolchain/cmsis/include/mpu_armv7.h:
+../../../../ESTC-NSDK/modules/nrfx/mdk/system_nrf52840.h:
+../../../../ESTC-NSDK/modules/nrfx/mdk/nrf52840_bitfields.h:
+../../../../ESTC-NSDK/modules/nrfx/mdk/nrf51_to_nrf52840.h:
+../../../../ESTC-NSDK/modules/nrfx/mdk/nrf52_to_nrf52840.h:
+../../../../ESTC-NSDK/modules/nrfx/mdk/compiler_abstraction.h:
+../../../../ESTC-NSDK/components/softdevice/mbr/headers/nrf_mbr.h:
+../../../../ESTC-NSDK/components/softdevice/mbr/headers/nrf_svc.h:
+../../../../ESTC-NSDK/components/libraries/util/sdk_macros.h:
+../../../../ESTC-NSDK/components/libraries/util/nrf_assert.h:
+../../../../ESTC-NSDK/components/libraries/util/sdk_errors.h:
+../../../../ESTC-NSDK/components/libraries/util/app_util_platform.h:
+../../../../ESTC-NSDK/components/libraries/util/app_error.h:
+/usr/arm-none-eabi/include/stdio.h:
+/usr/lib/gcc/arm-none-eabi/11.2.0/include/stdarg.h:
+/usr/arm-none-eabi/include/sys/types.h:
+/usr/arm-none-eabi/include/sys/_pthreadtypes.h:
+/usr/arm-none-eabi/include/machine/types.h:
+/usr/arm-none-eabi/include/sys/stdio.h:
+../../../../ESTC-NSDK/components/libraries/util/app_error_weak.h:
+../../../../ESTC-NSDK/components/libraries/util/nrf_assert.h:

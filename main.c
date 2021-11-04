e0dcb1c5c3d34a92f096443d07f65c4651dbac5b
TNTrol <sem.go@bk.ru>
2021-11-04 13:42:20 +0300

    changed blinking leds on gpio

--- main.c
+++ main.c
@@ -1,42 +1,4 @@
-/**
- * Copyright (c) 2014 - 2021, Nordic Semiconductor ASA
- *
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form, except as embedded into a Nordic
- *    Semiconductor ASA integrated circuit in a product or a software update for
- *    such product, must reproduce the above copyright notice, this list of
- *    conditions and the following disclaimer in the documentation and/or other
- *    materials provided with the distribution.
- *
- * 3. Neither the name of Nordic Semiconductor ASA nor the names of its
- *    contributors may be used to endorse or promote products derived from this
- *    software without specific prior written permission.
- *
- * 4. This software, with or without modification, must only be used with a
- *    Nordic Semiconductor ASA integrated circuit.
- *
- * 5. Any software provided in binary form under this license must not be reverse
- *    engineered, decompiled, modified and/or disassembled.
- *
- * THIS SOFTWARE IS PROVIDED BY NORDIC SEMICONDUCTOR ASA "AS IS" AND ANY EXPRESS
- * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY, NONINFRINGEMENT, AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL NORDIC SEMICONDUCTOR ASA OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
- * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
- * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- */
+
 /** @file
  *
  * @defgroup blinky_example_main main.c
@@ -52,48 +14,56 @@
 #include <stdint.h>
 #include "nrf_delay.h"
 #include "boards.h"
+#include "nrf_gpio.h"
 
 #define DEVICE_ID 6579
+#define DEVICE_LEDS {BSP_LED_0, BSP_LED_1, BSP_LED_2, BSP_LED_3}
 #define DEVICE_SIZE 4
 #define DEVICE_TIME 500
+#define MY_LED BSP_LED_0
 
 /**
  * @brief Function for application main entry.
  */
 
- void init_data(int *array)
+ void init_data(int (*array)[2])
  {
+    
     int temp = 0, number = DEVICE_ID;
+    static int arr[DEVICE_SIZE] = DEVICE_LEDS;
     for(int i = DEVICE_SIZE - 1; i >= 0 ; --i)
     {
         temp = number % 10;
         number /= 10;
-        array[i] = temp;
+        array[i][1] = temp;
+        array[i][0] = arr[i];
+        nrf_gpio_cfg_output(arr[i]);
+        nrf_gpio_pin_write(arr[i], 1);
     }
  }
 
  void make_blink(const int id_led, const int count)
  {
+     
     for (int j = 0; j < count; ++j)
     {
-        bsp_board_led_invert(id_led);
+        nrf_gpio_pin_write(id_led, 0);
         nrf_delay_ms(DEVICE_TIME);
-        bsp_board_led_invert(id_led);
+        nrf_gpio_pin_write(id_led, 1);
         nrf_delay_ms(DEVICE_TIME);
     }
  }
 
 int main(void)
 {
-    int blink_array[DEVICE_SIZE] = {0};
+    
+    int blink_array[DEVICE_SIZE][2] = {0};
     init_data(blink_array);
-    bsp_board_init(BSP_INIT_LEDS);
-
     while (true)
     {
         for(int i = 0; i < DEVICE_SIZE; ++i)
         {
-            make_blink(i, blink_array[i]);
+            make_blink(blink_array[i][0], blink_array[i][1]);
         }
     }
 }

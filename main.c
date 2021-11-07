95fa6798e8790d2588b3c176f1339a23621e1f6b
TNTrol <sem.go@bk.ru>
2021-11-07 15:06:40 +0300

    refactor code/ delete structure/ create new function/ add logging #9

--- main.c
+++ main.c
@@ -13,11 +13,22 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include "nrf_delay.h"
-#include "boards.h"
 #include "nrf_gpio.h"
 
+#include "nrf_log.h"
+#include "nrf_log_ctrl.h"
+#include "nrf_log_default_backends.h"
+#include "nrf_log_backend_usb.h"
+
+//#include "app_usbd.h"
+//#include "app_usbd_serial_num.h"
+
 #define DEVICE_ID 6579
-#define DEVICE_LEDS {NRF_GPIO_PIN_MAP(0, 6), NRF_GPIO_PIN_MAP(0, 8), NRF_GPIO_PIN_MAP(1, 9), NRF_GPIO_PIN_MAP(0, 12)}
+#define DEVICE_LED1 NRF_GPIO_PIN_MAP(0, 6)
+#define DEVICE_LED2 NRF_GPIO_PIN_MAP(0, 8)
+#define DEVICE_LED3 NRF_GPIO_PIN_MAP(1, 9)
+#define DEVICE_LED4 NRF_GPIO_PIN_MAP(0, 12)
+#define DEVICE_LEDS {DEVICE_LED1, DEVICE_LED2, DEVICE_LED3, DEVICE_LED4}
 #define DEVICE_SIZE 4
 #define DEVICE_TIME 500
 #define DEVICE_BUTTON NRF_GPIO_PIN_MAP(1, 6)
@@ -26,54 +37,72 @@
  * @brief Function for application main entry.
  */
 
- struct pin_blink_t
+ void init_data(const uint32_t size, uint32_t id, uint8_t *array)
  {
-     uint8_t pin;
-     uint8_t repeat;
- };
-
- typedef struct pin_blink_t PinBlink;
-
- void init_data(PinBlink *array)
- {
-    uint32_t temp = 0, number = DEVICE_ID;
-    static const uint8_t arr[DEVICE_SIZE] = DEVICE_LEDS;
-    for(int i = DEVICE_SIZE - 1; i >= 0 ; --i)
+    uint32_t temp = 0;
+    for(int i = size - 1; i >= 0 ; --i)
     {
-        temp = number % 10;
-        number /= 10;
-        array[i].repeat = temp;
-        array[i].pin = arr[i];
-        nrf_gpio_cfg_output(arr[i]);
-        nrf_gpio_pin_write(arr[i], 1);
+        temp = id % 10;
+        id /= 10;
+        array[i] = temp;
     }
-    nrf_gpio_cfg_input(DEVICE_BUTTON, NRF_GPIO_PIN_PULLUP);
  }
 
- void make_blink(const uint8_t pin)
+ void init_leds(const uint8_t *leds, const uint32_t size)
+ {
+    for(int i = 0; i < size; ++i)
+    {
+        nrf_gpio_cfg_output(leds[i]);
+        nrf_gpio_pin_write(leds[i], 1);
+    }
+ }
+
+ void make_blink(const uint8_t pin, const uint32_t time)
  {
     nrf_gpio_pin_write(pin, 0);
-    nrf_delay_ms(DEVICE_TIME);
+    nrf_delay_ms(time);
     nrf_gpio_pin_write(pin, 1);
-    nrf_delay_ms(DEVICE_TIME);
+    nrf_delay_ms(time);
+ }
+
+ void init_but(uint8_t button)
+ {
+     nrf_gpio_cfg_input(button, NRF_GPIO_PIN_PULLUP);
+ }
+
+ void init_log(void)
+ {
+    APP_ERROR_CHECK(NRF_LOG_INIT(NULL));
+    NRF_LOG_INFO("Starting up the test project with USB logging");
+    NRF_LOG_DEFAULT_BACKENDS_INIT();
  }
 
 int main(void)
 {
-    PinBlink blink_array[DEVICE_SIZE] = {0};
+    uint8_t blink_array[DEVICE_SIZE] = {0};
+    const uint8_t leds[DEVICE_SIZE] = DEVICE_LEDS;
     uint8_t index_led = 0, repeat = 0;
-    init_data(blink_array);
+    
+    init_data(DEVICE_SIZE, DEVICE_ID, blink_array);
+    init_leds(leds, DEVICE_SIZE);
+    init_but(DEVICE_BUTTON);
+    init_log();
+
     while (true)
     {
+        LOG_BACKEND_USB_PROCESS();
         if(! nrf_gpio_pin_read(DEVICE_BUTTON))
         {
-            if(blink_array[index_led].repeat <= repeat)
+            if(blink_array[index_led] <= repeat)
             {
                 index_led = index_led + 1 < DEVICE_SIZE ? index_led + 1 : 0;
                 repeat = 0;
-            }
-            make_blink(blink_array[index_led].pin);
+            }   
+            make_blink(leds[index_led], DEVICE_TIME);
             ++repeat;
+
+            NRF_LOG_INFO("LEDS #%d is blinking\n", index_led);
+            NRF_LOG_PROCESS();
         }
     }   
 }

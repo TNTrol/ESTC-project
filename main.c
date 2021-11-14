42695b4400f6fb3e61e07b174f1567687b271a74
TNTrol <sem.go@bk.ru>
2021-11-14 19:51:24 +0300

    add gpiote but bug with double click

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #include <stdbool.h>
 #include <stdint.h>
-#include "nrf_drv_systick.h"
 #include "nrf_delay.h"
 #include "gpio_module/gpio_module.h"
+#include "nrfx_systick.h"
+#include "nrfx_gpiote.h"
 
 
 #include "nrf_log.h"
@@ -16,7 +17,12 @@
 #define DEVICE_DISCRETE_DELAY 1U
 #define DEVICE_DELAY_BLINK 500
 #define DEVICE_TIME 10U
+#define DEVICE_BUTTON_DELAY_MIN 25000
+#define DEVICE_BUTTON_DELAY_MAX 100000
 
+static volatile bool double_click = false;
+static volatile bool freeze = false;
+static nrfx_systick_state_t time_state;
 
  void init_log(void)
  {
@@ -32,6 +38,36 @@
     invert_led(index_led);
     nrfx_systick_delay_us(DEVICE_TIME * time2);
  }
+ 
+void button_pressed_handler(nrfx_gpiote_pin_t pin, nrf_gpiote_polarity_t action)
+{
+    if(double_click)
+    {
+        nrfx_systick_get(&time_state);
+        double_click = true;
+        return;
+    }
+    else if(nrfx_systick_test(&time_state, DEVICE_BUTTON_DELAY_MIN) && !nrfx_systick_test(&time_state, DEVICE_BUTTON_DELAY_MAX))
+    {
+        freeze = !freeze;
+    }
+    double_click = false;
+    
+}
+
+ void init_gpiote(void)
+ {
+    nrfx_gpiote_init();
+    const nrfx_gpiote_in_config_t btn_gpiote_cfg = {
+        .sense = NRF_GPIOTE_POLARITY_HITOLO,
+        .pull = NRF_GPIO_PIN_PULLUP,
+        .is_watcher = false,
+        .hi_accuracy = false,
+        .skip_gpio_setup = true
+    };
+    nrfx_gpiote_in_init(BUTTON_1, &btn_gpiote_cfg, &button_pressed_handler);
+    nrfx_gpiote_in_event_enable(BUTTON_1, true);
+ }
 
 int main(void)
 {
@@ -44,12 +80,13 @@ int main(void)
     init_log();
     init_leds();
     init_button();
+    init_gpiote();
 
     while (true)
     {
         LOG_BACKEND_USB_PROCESS();
         NRF_LOG_PROCESS();
-        if(!get_value_button())
+        if(freeze)
         {
             if(DEVICE_DELAY_CICLE > time_cicle)  
             {

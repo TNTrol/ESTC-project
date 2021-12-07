835890e089d496624070f9d848cd9461fb9daa54
TNTrol <sem.go@bk.ru>
2021-12-07 16:16:18 +0300

    add checking flash memory #20

--- memory_module/memory_module.c
+++ memory_module/memory_module.c
@@ -1,5 +1,10 @@
 #include "memory_module.h"
 
+#include "nrf_log.h"
+#include "nrf_log_ctrl.h"
+#include "nrf_log_default_backends.h"
+#include "nrf_log_backend_usb.h"
+
 
 static volatile uint32_t m_address = 0;
 static uint8_t m_number_page = 0;
@@ -7,13 +12,28 @@ static uint8_t m_number_page = 0;
 uint32_t find_end(uint32_t address) // вынес для дебага
 {
     uint32_t temp = 0;
+    temp = *((uint32_t*) address);
+    if(((temp >> 24) & DATA_MASK) != DATA_MASK && ((temp >> 24) & DATA_MASK) != 0)
+    {
+        nrfx_nvmc_page_erase(address);
+    }
+    temp = 0;
     while (temp == 0)
     {
         temp = *((uint32_t*) address);
-        temp = DATA_MASK & (temp >> 24);
-        if(temp == 0)
+        if(temp != 0xFFFFFFFF)
         {
-            address += SIZE_STEP_FLASH;
+            temp = DATA_MASK & (temp >> 24);
+            if(temp == 0)
+            {
+                address += SIZE_STEP_FLASH;
+            }
+            else
+            {
+                address = START_FIRST_PAGE_FLASH + SIZE_PAGE_FLASH * (m_number_page);
+                nrfx_nvmc_page_erase(address);
+            }
+            temp = 0;
         }
         if(address >= START_FIRST_PAGE_FLASH + SIZE_PAGE_FLASH * (m_number_page + 1))
         {
@@ -44,6 +64,7 @@ bool read_data_in_flash(hsv_t* out_data)
 void write_data_in_flash(const hsv_t* in_data)
 {
     uint32_t out = 0U;
+    uint32_t temp;
     out |= in_data->h;
     out |= in_data->v << 8;
     out |= in_data->s << 16;
@@ -52,6 +73,12 @@ void write_data_in_flash(const hsv_t* in_data)
         m_number_page = (m_number_page + 1) % COUNT_PAGES_FLASH;
         nrfx_nvmc_page_erase(START_FIRST_PAGE_FLASH + SIZE_PAGE_FLASH * m_number_page);
         m_address = START_FIRST_PAGE_FLASH + SIZE_PAGE_FLASH * m_number_page;
+    }
+    temp = *((uint32_t*) m_address);
+    if(temp != 0xFFFFFFFF)
+    {
+        m_address = START_FIRST_PAGE_FLASH + SIZE_PAGE_FLASH * m_number_page;
+        nrfx_nvmc_page_erase(m_address);
     }
     nrfx_nvmc_word_write(m_address, out);
     m_address += SIZE_STEP_FLASH; 
